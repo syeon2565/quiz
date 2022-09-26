dea8820363a99f82261f9daf9d0335eca1bafb4c
seongyeon <ysy2565@naver.com>
2022-09-27 00:21:56 +0900

    💄 Create button component

---
+++ src/styles/DS/Button/Button.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { styled } from "@stitches/react";
+
+interface ButtonProps {
+  size?: "small" | "regular";
+  text: string;
+  buttonType?: "filled" | "outlined" | "disabled";
+  onClick?: () => void;
+}
+
+export const Button = ({
+  size = "regular",
+  text,
+  buttonType = "filled",
+  ...props
+}: ButtonProps) => {
+  return (
+    <Btn type="button" size={size} buttonType={buttonType} {...props}>
+      {text}
+    </Btn>
+  );
+};
+
+const Btn = styled("button", {
+  backgroundColor: "#BDD1FF",
+  borderRadius: "6px",
+  border: "none",
+  padding: "10px 15px",
+  cursor: "pointer",
+  color: "#1b1b1b",
+
+  variants: {
+    size: {
+      small: { width: "112px", fontSize: "14px" },
+      regular: { width: "200px", fontSize: "30px" },
+    },
+    buttonType: {
+      filled: {
+        backgroundColor: "#E9D8FD",
+        "&:hover": {
+          backgroundColor: "#B794F4",
+        },
+      },
+      outlined: {
+        backgroundColor: "transparent",
+        border: "1px solid #E9D8FD",
+        padding: "9px 14px",
+        "&:hover": {
+          backgroundColor: "#f3e9ff",
+        },
+      },
+      disabled: {
+        backgroundColor: "#c6c6c6",
+        cursor: "not-allowed",
+        color: "#eaeaea",
+      },
+    },
+  },
+});
