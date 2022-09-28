e3bbd1604dbfa62655705722213de9042aa5ca19
seongyeon <ysy2565@naver.com>
2022-09-28 20:39:04 +0900

    ✨ Complete quiz and result components

---
+++ src/components/Quiz.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import Layout from "./Layout";
+import { styled } from "@stitches/react";
+import { Button } from "../styles/DS/Button/Button";
+import useQuiz from "../hooks/useQuiz";
+import useQuizStore from "../store/useStore";
+import Result from "./Result";
+
+const Quiz = () => {
+  const { quizData, quizLoading } = useQuiz();
+
+  const handleNextBtn = useQuizStore(state => state.onNextQuiz);
+  const onSubmit = useQuizStore(state => state.onSubmit);
+  const currentNum = useQuizStore(state => state.currentQuizNum);
+
+  const [currentQuestion, setCurrentQuestion] = useState("");
+  const [answers, setAnswers] = useState<string[]>([]);
+
+  const clicked = useQuizStore(state => state.clicked);
+  const handleCheckAnswer = useQuizStore(state => state.checkedAnswer);
+  const randomIndex = Math.floor(Math.random() * 4);
+
+  useEffect(() => {
+    if (quizData) {
+      const { question, incorrect_answers, correct_answer } =
+        quizData.results[currentNum];
+      setCurrentQuestion(question);
+
+      const clonedAnswer = [...incorrect_answers];
+      clonedAnswer.splice(randomIndex, 0, correct_answer);
+      setAnswers(clonedAnswer);
+    }
+  }, [quizData, currentNum, quizLoading]);
+
+  return (
+    <Layout>
+      {quizLoading ? (
+        "Loading..."
+      ) : (
+        <>
+          {currentNum >= 10 ? (
+            <Result />
+          ) : (
+            <>
+              <QuizTitle>{`Q${currentNum + 1} ) ${currentQuestion}`}</QuizTitle>
+              {answers?.map((answer, i) => (
+                <StyledButton
+                  buttonType={clicked ? "disabled" : "outlined"}
+                  size="small"
+                  onClick={() => {
+                    handleCheckAnswer(i, randomIndex);
+                  }}
+                  key={i}
+                >
+                  {answer}
+                </StyledButton>
+              ))}
+
+              {clicked && (
+                <NextButton
+                  buttonType="filled"
+                  onClick={currentNum >= 9 ? onSubmit : handleNextBtn}
+                >
+                  {currentNum >= 9 ? "SUBMIT" : "NEXT"}
+                </NextButton>
+              )}
+            </>
+          )}
+        </>
+      )}
+    </Layout>
+  );
+};
+
+const QuizTitle = styled("div", {
+  margin: "80px 0",
+});
+
+const StyledButton = styled(Button, {
+  margin: "10px",
+  variants: {
+    checkAnswer: {
+      wrong: { color: "red" },
+      answer: { color: "blue" },
+    },
+  },
+});
+
+const NextButton = styled(Button, {
+  marginTop: "80px",
+});
+
+export default Quiz;
---
+++ src/components/Result.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Layout from "./Layout";
+import { styled } from "@stitches/react";
+import useQuizStore from "../store/useStore";
+import { RadialChart } from "react-vis";
+import { Button } from "../styles/DS/Button/Button";
+
+const Result = () => {
+  const score = useQuizStore(state => state.score);
+  const startTime = useQuizStore(state => state.startTime);
+  const endTime = useQuizStore(state => state.endTime);
+  const handleQuizStart = useQuizStore(state => state.ReStartQuiz);
+
+  const data = [
+    { angle: score, label: "정답" },
+    { angle: 10 - score, label: "오답" },
+  ];
+
+  return (
+    <Layout>
+      <Div color="answer"> 정답 {score} 개</Div>
+      <Div color="wrong">오답 {10 - score} 개</Div>
+      <Div color="time">
+        소요시간 {Math.floor((endTime - startTime) / 1000)} 초
+      </Div>
+      <RadialChart data={data} width={250} height={250} showLabels={true} />
+      <Button onClick={handleQuizStart}>다시 풀러가기</Button>
+    </Layout>
+  );
+};
+
+const Div = styled("div", {
+  fontSize: "22px",
+  variants: {
+    color: {
+      answer: { color: "#4643ff" },
+      wrong: { color: "#ff4747" },
+      time: { color: "#414141" },
+    },
+  },
+});
+
+export default Result;
