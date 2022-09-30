e1384aac950d10a314a3bca204cf3afbafbf0c03
seongyeon <ysy2565@naver.com>
2022-09-30 16:32:41 +0900

    ✅ Set test config and Add components test

---
+++ src/test/Layout.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Layout from "../components/Layout";
+import "@testing-library/jest-dom";
+
+describe("Layout test", () => {
+  it("render children", () => {
+    render(<Layout>test</Layout>);
+
+    expect(screen.getByText("test")).toBeVisible();
+  });
+});
---
+++ src/test/Result.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import Result from "../components/Result";
+import "@testing-library/jest-dom";
+
+describe("Result test", () => {
+  it("정답 갯수가 보인다", () => {
+    render(<Result />);
+
+    expect(screen.getByText(/^정답 \d+ 개$/)).toBeVisible();
+  });
+
+  it("오답 갯수가 보인다", () => {
+    render(<Result />);
+
+    expect(screen.getByText(/^오답 \d+ 개$/)).toBeVisible();
+  });
+
+  it("소요시간이 보인다", () => {
+    render(<Result />);
+
+    expect(screen.getByText(/^소요시간/)).toBeVisible();
+  });
+
+  it("다시 풀러가기 버튼이 보인다", () => {
+    render(<Result />);
+
+    expect(screen.getByText(/^다시 풀러가기/)).toBeVisible();
+  });
+});
