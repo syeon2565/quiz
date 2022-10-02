76d2a58387a26b178c6dfdc2c4846489f50dd985
seongyeon <ysy2565@naver.com>
2022-10-02 20:29:42 +0900

    ✅ Add main component test

--- src/test/IndexPage.test.tsx
+++ src/test/IndexPage.test.tsx
@@ -1,10 +1,11 @@
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import { queryClient } from "~pages/_app";
 import Index from "~pages/index";
 import "@testing-library/jest-dom";
+
+const queryClient = new QueryClient();
 
 describe("Index page test", () => {
   it("처음에 메인 컴포넌트가 보인다", () => {
